2d650df7ec8fa84689f4456cebbec7e4ecd14113
huilinn93 <huilinn93@gmail.com>
2024-05-11 00:28:15 +0800

    more things

---
+++ src/common/JackpotCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div>{{ randomWord }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    randomWord: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  border: 5px solid rgb(52, 250, 200);
+  padding: 1rem 0.5rem;
+  text-align: center;
+  margin: 0.5rem;
+  max-width: 15%;
+  width: 2rem;
+  color: white;
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
