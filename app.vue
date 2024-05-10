c1eae1a5d0f4d50eaa290348f4b20d987520a156
huilinn93 <huilinn93@gmail.com>
2024-05-11 00:28:04 +0800

    css things

--- app.vue
+++ app.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-    <h1>JACKPOT</h1>
-    <button @click="generateNewWords">SPIN!</button>
+  <div id="app">
+    <h2>BAPSKIER'S JACKPOT</h2>
     <div class="jackpotcards-container">
       <JackpotCard
         v-for="(randomWord, index) in randomWords"
         :key="index"
         :randomWord="randomWord"
-        />
-      </div>
+      />
     </div>
+    <button @click="generateNewWords">SPIN!</button>
+  </div>
 </template>
 
 <script>
-import JackpotCard from './common/JackpotCard.vue';
+import JackpotCard from './src/common/JackpotCard.vue';
 
 export default {
   components: {
@@ -28,7 +28,9 @@ export default {
   },
   methods: {
       generateNewWord() {
-          const resultIndex = Math.floor(Math.random() * 10);
+          let resultIndex = Math.floor(Math.random() * 10);
+          if((this.counter < 3 && resultIndex===5) || (this.counter < 2 && resultIndex===1)) resultIndex+=1
+
           return this.words[resultIndex];
       },
       generateNewWords() {
@@ -41,9 +43,29 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  color: red;
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
 .jackpotcards-container {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+}
+button {
+  width: 30%;
+  margin-top: 2rem;
+  background-color: #5842ff;
+  color: #ffffff;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background-image: url('./src/image/image.jpg');
+  background-size: cover;
 }
 </style>
--- common/Title.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    {{ title }}
-  </div>
-</template>
-
-<script setup lang="ts">
-const title = 'HELLOOO' 
-</script>
